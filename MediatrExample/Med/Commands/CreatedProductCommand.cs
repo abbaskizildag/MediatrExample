3da18847f1886609e5a7b6c32912371a9bd5c6f1
Ali Kızıldağ <ali.kizildag@cottgroup.com>
2022-10-03 22:26:55 +0300

    Add project files.

---
+++ MediatrExample/Med/Commands/CreatedProductCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace MediatrExample.Med.Commands
+{
+    public class CreatedProductCommand: IRequest<Guid>
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Value { get; set; }
+        public class CreateProductCommandHandler : IRequestHandler<CreatedProductCommand, Guid>
+        {
+            public Task<Guid> Handle(CreatedProductCommand request, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(Guid.NewGuid());
+            }
+        }
+    }
+}
